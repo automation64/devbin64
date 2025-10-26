c74c007b257af3fb7ac137bd4856c906ced3c5b7
SerDigital64 <serdigital64@gmail.com>
2025-10-26 17:34:23 +0100

    dev: test cicd

--- test/lib/runtime.bash
+++ test/lib/runtime.bash
@@ -2,7 +2,7 @@
 
 at64root="${HOME}/at64"
 export BL64_LIB_CICD="YES"
-export TEST_TARGET="${HOME}/repo"
+export TEST_TARGET="${HOME}/test-repo"
 export TEST_INSTALL_TARGET="${at64root}/devbin64"
 export DEV_BOOTSTRAP_PATH_ROOT="$TEST_INSTALL_TARGET"
 export TEST_SOURCE_LIB=''
