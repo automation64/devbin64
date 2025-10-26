7349280b47148d01a2f4410e199c3f7d9736f274
SerDigital64 <serdigital64@gmail.com>
2025-10-26 17:46:03 +0100

    dev: test cicd

--- test/lib/install.bash
+++ test/lib/install.bash
@@ -7,4 +7,4 @@ mkdir -p \
   cp -r "${TEST_SOURCE_LIB}/lib" "$TEST_INSTALL_TARGET" &&
   cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" &&
   cd "$TEST_TARGET" &&
-  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" #>/dev/null
+  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" >/dev/null
