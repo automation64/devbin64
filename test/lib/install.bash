9e27bc5795253df0b09443d752ce6d299e70b695
SerDigital64 <serdigital64@gmail.com>
2025-10-26 13:29:49 +0100

    test: update

--- test/lib/install.bash
+++ test/lib/install.bash
@@ -2,13 +2,20 @@
 
 at64root="${HOME}/at64"
 install_target="${at64root}/devbin64"
+source_lib=''
 export DEV_BOOTSTRAP_PATH_ROOT="$install_target"
+
+if [[ -v DEV_TEST_BASH_CONTAINER_ENVIRONMENT ]]; then
+  source_lib="$DEV_ENV_BASH_CONTAINER_PATH_SRC"
+else
+  source_lib='src'
+fi
 
 mkdir -p \
   "$install_target" \
   "${install_target}/bin" \
   "$TEST_TARGET" &&
-  cp -r "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/lib" "$install_target" &&
-  cp "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/lib/devbin64/bin/dev-bootstrap" "${install_target}/bin" &&
+  cp -r "${source_lib}/lib" "$install_target" &&
+  cp "${source_lib}/lib/devbin64/bin/dev-bootstrap" "${install_target}/bin" &&
   cd "$TEST_TARGET" &&
-  "${install_target}/bin/dev-bootstrap" > /dev/null 2>&1
+  "${install_target}/bin/dev-bootstrap" >/dev/null 2>&1
