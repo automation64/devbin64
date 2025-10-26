4a17171ecf6925e65b908c02ccd4b728e672b443
SerDigital64 <serdigital64@gmail.com>
2025-10-26 16:44:05 +0100

    test: update

--- test/lib/install.bash
+++ test/lib/install.bash
@@ -7,4 +7,4 @@ mkdir -p \
   cp -r "${TEST_SOURCE_LIB}/lib" "$TEST_INSTALL_TARGET" &&
   cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" &&
   cd "$TEST_TARGET" &&
-  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" >/dev/null 2>&1
+  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" >/dev/null
