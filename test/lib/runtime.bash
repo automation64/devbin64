60106b257cac2ae1d214feb2596a87054d144640
SerDigital64 <serdigital64@gmail.com>
2026-01-18 21:54:43 +0100

    refact: verbose

--- test/lib/runtime.bash
+++ test/lib/runtime.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
 at64root="${HOME}/at64"
 export BL64_LIB_CICD="YES"
 export TEST_TARGET="${HOME}/test-repo"
 export TEST_INSTALL_TARGET="${at64root}/devbin64"
 export DEV_BOOTSTRAP_PATH_ROOT="$TEST_INSTALL_TARGET"
 export TEST_SOURCE_LIB='src'
+set +x
