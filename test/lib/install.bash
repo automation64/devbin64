3a10ba82f56b32ec159d034a33ac1cfb6087880c
SerDigital64 <serdigital64@gmail.com>
2026-01-19 13:00:44 +0100

    ci: test

--- test/lib/install.bash
+++ test/lib/install.bash
@@ -6,16 +6,21 @@ if [[ -d "$TEST_INSTALL_TARGET" ]]; then
   rm -Rf "$TEST_INSTALL_TARGET"
 fi
 
+if [[ -d "$TEST_TARGET" ]]; then
+  rm -Rf "$TEST_TARGET"
+fi
+
 # Install DevBin64 in test environment
 mkdir -p \
   "$TEST_INSTALL_TARGET" \
   "${TEST_INSTALL_TARGET}/bin" \
   "$TEST_TARGET" &&
   cp -r "${TEST_SOURCE_LIB}/lib" "$TEST_INSTALL_TARGET" &&
-  cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" &&
-  cd "$TEST_TARGET" ||
+  cp "${TEST_SOURCE_LIB}/lib/devbin64/bin/dev-bootstrap" "${TEST_INSTALL_TARGET}/bin" ||
   exit $?
 
 [[ -n "$DEV_CICD_DEBUG_BASH" ]] && set +x
 
-"${TEST_INSTALL_TARGET}/bin/dev-bootstrap"
+cd "$TEST_TARGET" &&
+  "${TEST_INSTALL_TARGET}/bin/dev-bootstrap" &&
+  ls bin
