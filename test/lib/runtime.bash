5ae2290112279365a983c06b8b33e9003a0690fc
bl64_vcs_run_git <nouser@nodomain>
2026-06-21 17:39:27 +0200

    real: promote 15.0.0

--- test/lib/runtime.bash
+++ test/lib/runtime.bash
@@ -2,9 +2,12 @@
 
 [[ -n "$DEV_CICD_DEBUG_BASH" ]] && set -x
 
-declare at64root="${HOME}/at64"
+declare at64root="/tmp/at64"
+
+[[ -z "$HOME" ]] && exit 1
+
 export BL64_LIB_CICD="YES"
-export TEST_TARGET="${HOME}/test-repo"
+export TEST_TARGET="/tmp/test-repo"
 export TEST_INSTALL_TARGET="${at64root}/devbin64"
 export DEV_BOOTSTRAP_PATH_ROOT="$TEST_INSTALL_TARGET"
 export TEST_SOURCE_LIB='src'
